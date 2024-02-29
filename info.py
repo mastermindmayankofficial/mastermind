ba32ca621a3dbdc7f1210979d69884a7b965c25c
mastermindmayank <88104889+mastermindmayankofficial@users.noreply.github.com>
2024-02-29 18:02:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,7 +23,7 @@ PICS = (environ.get('PICS', 'https://graph.org/file/efc2a34ed5c75d44c1ed5.jpg'))
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6791777555').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002094710881').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002047040638').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
